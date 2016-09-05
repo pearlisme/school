cae0669374b2eca82d36c7772b003475ee28a9d7
pearl <r.muthubharathi@live.in>
2016-09-05 19:17:47 +0000

    skeleton 1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Express' });
 });
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Welcome to SchoolMGMT' });
+});
+
 
 module.exports = router;
